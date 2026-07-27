c7bd1100cfa3aa4e91558ddd1a1bbce292e9f77e
Steven Chin <skingtw@gmail.com>
2026-07-27 23:12:52 +0800

    2 vregs chked

--- sw/test_gen/matmul8_vec_test.c
+++ sw/test_gen/matmul8_vec_test.c
@@ -11,7 +11,7 @@ extern void matmul8_vec(const volatile uint32_t *a,
 #define BS 32
 #define WORDS_PER_VREG 32
 //#define NUM_VREGS 32
-#define NUM_VREGS 1
+#define NUM_VREGS 2
 
 static volatile uint32_t *const DONE_MMIO       = (volatile uint32_t *)0xFFFF0000u;
 static volatile uint32_t *const COMP_START_MMIO = (volatile uint32_t *)0xFFFF0004u;
@@ -201,13 +201,13 @@ for (int i = 0; i < WORDS_PER_VREG * NUM_VREGS; i++) {
   // ==========================================
   // BRING-UP TEST 2: Register v1 Verification
   // ==========================================
-//  load_act_scales(act_scales);
-//  load_w_scales(weight_scales);
-//  load_v1(&mat_a[32]);              // Loads v1 (Words 32 to 63)
+  load_act_scales(act_scales);
+  load_w_scales(weight_scales);
+  load_v1(&mat_a[32]);              // Loads v1 (Words 32 to 63)
   //mac_zz();
-//  mac_mem_test_v1(weights);         // Multiplies v1 by weights[32..63]
-//  mac_as();
-//  mac_ws();
+  mac_mem_test_v1(weights);         // Multiplies v1 by weights[32..63]
+  mac_as();
+  mac_ws();
   //chk = mac_out(0, 0, 2);
 mac_bias(31,7,7,0x3fc0); 
 
