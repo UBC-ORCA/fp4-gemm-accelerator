44f5a44713b55a5e004eec793fa954d704b9bf8e
KenrickMH <kenrickmarcellhaditio@gmail.com>
2026-08-03 03:52:12 -0700

    [SNAPSHOT SCALE_OPT] update core files + opt scale unit :

--- rtl/rtl/e8m0_scale.sv
+++ rtl/rtl/e8m0_scale.sv
@@ -9,16 +9,20 @@ import mx_pkg::*;
     output bf16_t      bf16_out
 );
 
-    logic signed [9:0] delta;
-    logic signed [9:0] calculated_exp;
+    logic signed [9:0] sgn_scale_a;
+    logic signed [9:0] sgn_scale_b;
+    logic signed [9:0] sgn_bf16_exp;
+    logic signed [9:0] calculated_exp;  
+    
+    assign sgn_scale_a = {2'b0, scale_a};
+    assign sgn_scale_b = {2'b0, scale_b};
+    assign sgn_bf16_exp = {2'b0, bf16_in.exp};
 
-    // Compute delta = scaleA + scaleB - 254
-    assign delta = $signed({2'b0, scale_a}) + $signed({2'b0, scale_b}) - 10'd254;
-    assign calculated_exp = $signed({2'b0, bf16_in.exp}) + delta;
+    /* Use a higher adder fan-in to save more area + timing latency */
+    assign calculated_exp = sgn_scale_a + sgn_scale_b + sgn_bf16_exp - 10'sd254;
+    assign bf16_out.sign = bf16_in.sign;
 
     always_comb begin
-        // 1. Pass-through sign
-        bf16_out.sign = bf16_in.sign;
 
         // 2. Zero-Bypass handling & special cases
         if (bf16_in.exp == 8'h0 && bf16_in.mant == 7'h0) begin
@@ -30,12 +34,13 @@ import mx_pkg::*;
             bf16_out.exp  = 8'hFF;
             bf16_out.mant = bf16_in.mant;
         end
-        // 3. Overflow handling
+        // 3. Overflow handling, not sure if neccesary, 
         else if (calculated_exp >= 10'sd255) begin
             bf16_out.exp  = 8'hFF;  // Infinity encoding
             bf16_out.mant = 7'h0;
         end
         // 4. Underflow handling (Flush-to-Zero)
+        // uhhh....??
         else if (calculated_exp <= 10'sd0) begin
             bf16_out.exp  = 8'h0;
             bf16_out.mant = 7'h0;
