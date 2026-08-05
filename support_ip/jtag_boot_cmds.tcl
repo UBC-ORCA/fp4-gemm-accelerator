491ae1fe0f0b095fa387b0e9750cb9ae2f3267b8
KenrickMH <kenrickmarcellhaditio@gmail.com>
2026-08-05 03:29:58 -0700

    Add support IP

---
+++ support_ip/jtag_boot_cmds.tcl
@@ -0,0 +1,44 @@
+connect
+tar 1
+rst -por
+exec sleep 1
+
+targets -set -filter {name =~ "PSU"}
+mwr 0xffca0038 0x1ff
+rst -srst
+exec sleep 1
+
+targets -set -nocase -filter {name =~ "*PS TAP*"}
+fpga /local/disk2/blankp1/mataccel/fp4-gemm-accelerator/rtl/build/openhwgroup_cve2_cve2_top_0.1/default-vivado/openhwgroup_cve2_cve2_top_0.1.runs/impl_1/accelerator_top.bit
+exec sleep 1
+
+# Write pmufw
+targets -set -nocase -filter {name =~ "*PSU*"}
+mask_write 0xFFCA0038 0x1C0 0x1C0
+targets -set -nocase -filter {name =~ "*MicroBlaze PMU*"}
+dow /local/disk2/blankp1/vitis/mataccel/export/mataccel/sw/mataccel/boot/pmufw.elf
+con
+exec sleep 1
+targets -set -nocase -filter {name =~ "*PSU*"}
+mask_write 0xFFCA0038 0x1C0 0x0
+
+# Configuring PSU
+targets -set -nocase -filter {name =~ "*APU*"}
+mwr 0xffff0000 0x14000000
+mask_write 0xFD1A0104 0x501 0x0
+targets -set -nocase -filter {name =~ "PSU"}
+source /local/disk2/blankp1/mataccel/fp4-gemm-accelerator/rtl/build/openhwgroup_cve2_cve2_top_0.1/default-vivado/openhwgroup_cve2_cve2_top_0.1.gen/sources_1/bd/ps/ip/ps_zynq_ultra_ps_e_0_0/psu_init.tcl
+psu_init
+after 500
+psu_post_config
+after 500
+psu_ps_pl_reset_config
+after 500
+psu_ps_pl_isolation_removal
+after 500
+
+#download fsbl
+# targets -set -nocase -filter {name =~ "*Cortex-A53 #0*"}
+# dow /local/disk2/blankp1/vitis/mataccel/export/mataccel/sw/mataccel/boot/fsbl.elf
+# con
+# exec sleep 1
